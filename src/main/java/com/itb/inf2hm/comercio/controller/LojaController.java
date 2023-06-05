13dff75119040e6438236e5b5023570f375714aa
OttoFidelis <ottofidelis@gmail.com>
2023-06-05 09:14:39 -0300

    Início controle de versão

---
+++ src/main/java/com/itb/inf2hm/comercio/controller/LojaController.java
@@ -0,0 +1,30 @@
+package com.itb.inf2hm.comercio.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itb.inf2hm.comercio.model.Produto;
+
+
+@Controller
+@RequestMapping("/comercio/produtos")
+public class LojaController {
+
+	List<Produto> produtos = new ArrayList<Produto>();
+	
+	@GetMapping("/listar")
+	public String listarProdutos(Model model) {
+		
+		Produto produto = new Produto(); 
+		produto.setNome("Televisor 70' Samsung");
+		produto.setCodigoBarras("1321454357875334566532");
+		produto.setPreco(6541.25);
+		
+		return "produtos";
+	}
+}
---
+++ src/main/java/com/itb/inf2hm/comercio/model/Produto.java
@@ -0,0 +1,37 @@
+package com.itb.inf2hm.comercio.model;
+
+public class Produto {
+	
+	private long id;
+	private String nome;
+	private String codigoBarras;
+	private double preco;
+    //public  :  Acesso liberado para todas as classes 
+	//private :  Propriedade ( atributo ou método ) só pode ser acessado 
+	//           dentro da própria classe
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+       
+}
