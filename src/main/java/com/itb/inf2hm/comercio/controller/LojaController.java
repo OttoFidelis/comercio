4bd2ba6210f9d6e79f98a61dca23278335cee4b3
Otto Fidelis <ottofidelis@gmail.com>
2023-06-13 11:49:44 -0300

    Ajustes no controller loja

--- src/main/java/com/itb/inf2hm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2hm/comercio/controller/LojaController.java
@@ -15,16 +15,34 @@ import com.itb.inf2hm.comercio.model.Produto;
 @RequestMapping("/comercio/produtos")
 public class LojaController {
 
-	List<Produto> produtos = new ArrayList<Produto>();
+	List<Produto> listaDeProdutos = new ArrayList<Produto>();
 	
 	@GetMapping("/listar")
 	public String listarProdutos(Model model) {
 		
 		Produto produto = new Produto(); 
+		produto.setId(1231);
 		produto.setNome("Televisor 70' Samsung");
 		produto.setCodigoBarras("1321454357875334566532");
 		produto.setPreco(6541.25);
 		
+		Produto produto2 = new Produto();
+		produto2.setId(3213);
+		produto2.setNome("Playstation");
+		produto2.setCodigoBarras("12345654123654123465112");
+		produto2.setPreco(10000.00);
+		
+		Produto produto3 = new Produto();
+		produto3.setId(76543);
+		produto3.setNome("Computador");
+		produto3.setCodigoBarras("123456597587645741476523");
+		produto3.setPreco(2345.99);
+		//Adicionando os produtos no ArrayList
+		
+		listaDeProdutos.add(produto);
+		listaDeProdutos.add(produto2);
+		listaDeProdutos.add(produto3);
+		model.addAttribute("produtos",listaDeProdutos);
 		return "produtos";
 	}
 	@GetMapping("/novo-produto")
